e476f369751e9013e1d191914abf2ee50bb90c1c
Joe Newman <makerslaptop23@Makerss-MacBook-Pro-4.local>
2015-02-24 16:50:18 +0000

    links added

--- server.rb
+++ server.rb
@@ -1,15 +1,20 @@
 require 'data_mapper'
+require 'sinatra'
 
 env = ENV["RACK_ENV"] || "development"
 
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
-require './lib/link'
+require 'link'
 
-
-
+get '/' do
+  @links = Link.all
+  erb :index
+end
 
 
 DataMapper.finalize
 
 DataMapper.auto_upgrade!
+
+
