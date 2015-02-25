293f44d830f21f3ed9a0c3b81689a4a49356bb0d
Joe Newman <makerslaptop23@Makerss-MBP.home>
2015-02-25 10:02:01 +0000

    user password functionality

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'database_cleaner'
 gem 'capybara'
+gem 'bcrypt-ruby'
 
 group :test do
   gem 'rspec'
--- server.rb
+++ server.rb
@@ -2,10 +2,14 @@ require 'data_mapper'
 require 'sinatra'
 require 'link'
 require 'tag'
+require 'user'
 
 env = ENV["RACK_ENV"] || "development"
 
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+
+enable :sessions
+set :session_secret, 'super secret'
 
 get '/' do
   @links = Link.all
@@ -20,6 +24,29 @@ post '/links' do
   end
   Link.create(:url => url, :title => title, :tags => tags)
   redirect to('/')
+end
+
+get '/tags/:text' do
+  tag = Tag.first(:text => params[:text])
+  @links = tag ? tag.links : []
+  erb :index
+end
+
+get '/users/new' do
+  erb :"users/new"
+end
+
+post '/users' do
+  user = User.create(:email => params[:email],
+              :password => params[:password])
+  session[:user_id] = user.id
+  redirect to('/')
+end
+
+helpers do
+  def current_user
+    @current_user ||=User.get(session[:user_id]) if session[:user_id]
+  end
 end
 
 DataMapper.finalize
