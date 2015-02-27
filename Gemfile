a93a434b72a960b881446ffd37d6f28f45abdf9d
Joe Newman <makerslaptop23@Makerss-MacBook-Pro-4.local>
2015-02-27 12:14:33 +0000

    gem file changes

--- Gemfile
+++ Gemfile
@@ -4,20 +4,24 @@ ruby '2.1.3'
 
 gem 'sinatra'
 gem 'data_mapper'
-#gem 'dm-postgres-adapter'
 gem 'bcrypt-ruby'
 gem 'rack-flash3'
 gem 'sinatra-partial'
 gem 'poltergeist'
 gem 'heroku'
 gem 'bcrypt'
-#gem 'pg'
+#gem 'rspec'
+#gem 'database_cleaner'
+#gem 'capybara'
+#gem "pg"
+#gem "dm-postgres-adapter"
 
+  
 group :development, :test do
   gem 'rspec'
   gem 'database_cleaner'
   gem 'capybara'
-
+end
 
 group :production do
     gem "pg"
--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -8,13 +8,14 @@ require_relative 'models/user'
 #     DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
 # end
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+#DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
+DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_CRIMSON_URL'] || 'postgres://localhost/bookmark_manager_#{env}')
 
 
 DataMapper.finalize
 
 # require 'data_mapper'
-# DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/bookmark_manager_#{env}')
+# 
 
 #DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://user:password@hostname/data/mydatabase.db')
