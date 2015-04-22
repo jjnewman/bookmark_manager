b77e20b393450b8a4494bfb410ac695805d157ba
Joe Newman <jj_newman@hotmail.com>
2015-04-22 22:17:25 +0100

    Update server.rb

--- app/server.rb
+++ app/server.rb
@@ -7,7 +7,6 @@ require_relative 'models/tag'
 require_relative 'models/user'
 require_relative 'helpers/application'
 require_relative 'data_mapper_setup'
-
 require_relative 'controllers/users'
 require_relative 'controllers/sessions'
 require_relative 'controllers/links'
@@ -18,17 +17,4 @@ enable :sessions
 set :session_secret, 'super secret'
 use Rack::Flash
 set :partial_template_engine, :erb
-
 set :public_folder, Proc.new { File.join(root, '..', "public") }
-#set :views, Proc.new { File.join(root, "..", "views") }
-
-
-
-
-
-
-
-
-
-
-
