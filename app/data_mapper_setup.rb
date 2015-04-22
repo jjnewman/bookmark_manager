c63b79f4ec7f8bd9437e6b7d5b8fb2bed2a27822
Joe Newman <jj_newman@hotmail.com>
2015-04-22 22:16:49 +0100

    Update data_mapper_setup.rb

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -4,18 +4,5 @@ require_relative 'models/link'
 require_relative 'models/tag'
 require_relative 'models/user'
 
-# configure :development do
-#     DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
-# end
-
-#DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
-
 DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_CRIMSON_URL'] || 'postgres://localhost/bookmark_manager_#{env}')
-
-
 DataMapper.finalize
-
-# require 'data_mapper'
-# 
-
-#DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://user:password@hostname/data/mydatabase.db')
